a8b23708228c7359bc28a57e853b12a8715193ea
Marcel Samyn <dev@mrcl.io>
2016-11-04 17:25:11 +0100

    Do reservation via the Salesforce API

--- server/post-to-team.ts
+++ server/post-to-team.ts
@@ -10,14 +10,21 @@ import { Campaign } from '../src/app/reservations/models/campaign';
 let request = Observable.bindNodeCallback<{}, any>(require('request'));
 
 export const postToTeam = (settings: any, reservation: Reservation, production: Observable<Campaign>): Observable<any> => {
+  let nTickets = reservation.Tickets.reduce((sum, t) => sum + t.amount, 0);
   return production.mergeMap(production => {
     return request({
       url: settings.team.zosw.hookUrl,
       method: 'POST',
       json: {
-        title: "Joepie, " + reservation.FirstName + " " + reservation.LastName + " heeft gereserveerd voor "
-          + production.Name + "!",
-        text: "Dat zijn dan " + reservation.Tickets.reduce((sum, t) => sum + t.amount, 0) + " nieuwe tickets."
+        summary: 'Nieuwe reservatie voor ' + nTickets + ' tickets.',
+        title: 'Joepie, nieuwe reservatie!',
+        sections: [
+          {
+            activityTitle: 'Op naam van ' + reservation.FirstName + ' ' + reservation.LastName,
+            activitySubtitle: production.Name,
+            activityText: '' + nTickets + ' nieuwe plaatsen'
+          }
+        ]
       }
     });
   });
