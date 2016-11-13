fa06b74f1877799657a3a096ca4aa1c1a37dbdca
Marcel Samyn <dev@mrcl.io>
2016-11-13 09:53:26 +0100

    Improve reservation form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { NgSemanticModule } from 'ng-semantic';
 
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppData } from './app-data';
@@ -23,6 +24,7 @@ import { ReservationsModule } from './reservations/reservations.module';
     HttpModule,
     // InMemoryWebApiModule.forRoot(AppData),
     MaterialModule.forRoot(),
+    NgSemanticModule,
 
     ReservationsModule
   ],
